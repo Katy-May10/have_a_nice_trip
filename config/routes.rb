ffc979477356ebb19550f706422b6551b3307368
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-12 15:57:52 +0900

    ルーティング設定　トップページにrootを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "itineraries#index"
+  root "pages#home"
   resources :itineraries, only: [:index, :new, :create, :show] do
     resources :plans, only: [:create, :update]
   end
