10577cf2196c63c4ef954a8a60f77ab78bd11a03
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-13 21:44:34 +0900

    User model アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :itineraries
+  has_many :plans
+  
   validates :nickname, presence: true
 end
