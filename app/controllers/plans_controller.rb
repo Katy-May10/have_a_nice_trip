3718cb1e449557403fdf86cc8992b86cd20943d2
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-13 21:43:23 +0900

    plans controller リダイレクト先のパス変更・icon_idを一時削除

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,17 +1,21 @@
 class PlansController < ApplicationController
   def create
     @plan = Plan.new(plan_params)
-    redirect_to itinerary_path(@itinerary)
+    if @plan.save
+      redirect_to itinerary_path(@plan.itinerary.id)
+    else
+      render :show, status: :unprocessable_entity
+    end
   end
 
   def update
     @plan = Plan.find[:id]
-    redirect_to itinerary_path(@itinerary)
+    redirect_to itinerary_path(@plan.itinerary.id)
   end
 
   private
 
   def plan_params
-    params.require(:plan).permit(:icon_id, :time, :content).merge(user_id: current_user.id, itinerary_id: params[:itinerary_id])
+    params.require(:plan).permit(:time, :content).merge(user_id: current_user.id, itinerary_id: params[:itinerary_id])
   end
 end
