0980fd098d04934fb412d5ab57f83937ecce92f2
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-13 21:42:35 +0900

    Itineraries controller user_idをマージ

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -16,11 +16,22 @@ class ItinerariesController < ApplicationController
     @itinerary = Itinerary.find[:id]
     @plan = Plan.new
     @plans = @itinerary.plans
+    if @itinerary.save
+      redirect_to itinerary_path(@itinerary.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @itinerary = Itinerary.find(params[:id])
+    @plan = Plan.new
+    @plans = @itinerary.plans.includes(:user)
   end
 
   private
 
   def itinerary_params
-    params.require(:itinerary).permit(:location, :date)
+    params.require(:itinerary).permit(:location, :date).merge(user_id: current_user.id)
   end
 end
