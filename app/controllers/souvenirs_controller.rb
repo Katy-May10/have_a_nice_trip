8db575b1f9834ee005b67fb6c0410e01c9cec2af
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-16 22:36:29 +0900

    Update sourvenirs_controller.rb

--- app/controllers/souvenirs_controller.rb
+++ app/controllers/souvenirs_controller.rb
@@ -1,2 +1,20 @@
 class SouvenirsController < ApplicationController
+  def index
+    @souvenirs = Souvenir.all
+    @souvenir = Souvenir.new
+  end
+
+  def create
+    @souvenir = Souvenir.new(sourvenir_params)
+    if @souvenir.save
+      redirect_to souvenirs_path(@souvenir.id)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def sourvenir_params
+    params.require(:souvenir).permit(:who).merge(user_id: current_user.id)
+  end
 end
