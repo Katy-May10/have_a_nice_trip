b13a29a80c9c95d6be7c2265d4003f73955ac0d4
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-16 22:23:29 +0900

    rails db:migrate

--- db/migrate/20241116131512_create_souvenirs.rb
+++ db/migrate/20241116131512_create_souvenirs.rb
@@ -1,7 +1,8 @@
 class CreateSouvenirs < ActiveRecord::Migration[7.0]
   def change
     create_table :souvenirs do |t|
-
+      t.string :who, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
