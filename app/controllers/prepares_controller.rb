b940c6d16894b9d99f8b44477048778c002183dc
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-16 18:25:01 +0900

    Update prepares_controller.rb

--- app/controllers/prepares_controller.rb
+++ app/controllers/prepares_controller.rb
@@ -1,5 +1,14 @@
 class PreparesController < ApplicationController
   def index
-    
+    @prepares = Prepare.all
+  end
+
+  def create
+    @prepare = Prepare.new(prepare_params)
+    if @prepare.save
+      redirect_to prepares_path(@prepare.id)
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 end
