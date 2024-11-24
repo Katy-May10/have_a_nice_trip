6258caefc1330ecd93ec73c4b7d92cd6dd0ce614
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-24 22:08:19 +0900

    Add actions index, new, create, show

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,17 +1,27 @@
 class RecordsController < ApplicationController
   def index
-    
+    @records = Record.all
   end
 
   def new
-    
+    @record = Record.new
   end
 
   def create
-    
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to records_path(@record.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-    
+    @record = Record.find(params[:id])
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:title).merge(user_id: current_user.id)
   end
 end
