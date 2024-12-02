90bfd32d1097338a5cc2886f818aa000088257d8
Katy-May10 <jinyefeng1994@gmail.com>
2024-12-02 17:06:05 +0900

    Update models/plan.rb (enum)

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,5 +2,19 @@ class Plan < ApplicationRecord
   belongs_to :itinerary
   belongs_to :user
 
+  enum icon_id: {
+    plane: 0,
+    train: 1,
+    car: 2,
+    walk: 3,
+    restaurant: 4,
+    cafe: 5,
+    sightseeing: 6,
+    shopping: 7,
+    hotel: 8,
+    hotspring: 9,
+    rest: 10,
+  }
+
   validates :content, presence: true
 end
